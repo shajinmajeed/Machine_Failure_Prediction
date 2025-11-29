9c2404c1bd0ef82d813496a32ff3447a174d6501
shajinmajeed <97589758+shajinmajeed@users.noreply.github.com>
2025-11-29 11:20:02 +0530

    Update hosting.py

--- machine_failure_prediction/hosting/hosting.py
+++ machine_failure_prediction/hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="week_3_mls/deployment",     # the local folder containing your files
+    folder_path="machine_failure_prediction/deployment",     # the local folder containing your files
     repo_id="shajinmajeed/Machine-Failure-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
